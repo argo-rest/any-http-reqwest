ae388d23182a30269960c10084b2993edb3fd842
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-11-09 15:47:06 +0000

    Initial import from theseus repo

---
+++ src/http.js
@@ -0,0 +1,60 @@
+import reqwest from 'reqwest';
+
+
+function getHeaders(request) {
+  return {
+    // FIXME: can't read Location?
+    // 'Location':     request.getResponseHeader('Location'),
+    'Content-Type': request.getResponseHeader('Content-Type')
+  };
+}
+
+function dispatch(method, uri, data) {
+  // Note: don't use reqwest's promises that are not A+-compliant
+  return new Promise((resolve, reject) => {
+    var req = reqwest({
+      url:     uri,
+      method:  method,
+      type:    'json',
+      data:    data,
+      // FIXME: not for GET though?
+      // FIXME: or argo?
+      contentType: 'application/json',
+      headers: {
+        // FIXME: should be passed in as argument to make adapter more generic
+        'Accept': 'application/vnd.argo+json'
+      },
+// TODO: optional:
+      crossOrigin: true,
+      //withCredentials: true,
+      success: (body) => resolve({uri: uri, body: body, status: req.status, headers: getHeaders(req.request)}),
+      // FIXME: parse response iff json content-type
+      error:   ()     => reject( {uri: uri, body: req.request.response, status: req.status, headers: getHeaders(req.request)})
+    });
+  });
+}
+
+
+export class Http {
+
+  get(uri, params, implemOptions) {
+    return dispatch('get', uri, params);
+  }
+
+  post(uri, data, implemOptions) {
+    return dispatch('post', uri, JSON.stringify(data));
+  }
+
+  put(uri, data, implemOptions) {
+    return dispatch('put', uri, JSON.stringify(data));
+  }
+
+  patch(uri, data, implemOptions) {
+    return dispatch('patch', uri, JSON.stringify(data));
+  }
+
+  delete(uri, implemOptions) {
+    return dispatch('delete', uri);
+  }
+
+}
