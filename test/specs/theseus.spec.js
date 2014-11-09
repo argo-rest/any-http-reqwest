03d0bd946a5d95ee70bf43aa93d1b86dbd15d123
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-11-09 21:17:07 +0000

    Use Chrome to run tests, so that they actually work

--- test/specs/theseus.spec.js
+++ test/specs/theseus.spec.js
@@ -1,10 +1,35 @@
-import theseus from './index';
+import {Http} from '../../index';
 
-describe('theseus', function() {
+describe('Http', () => {
 
     // TODO: replace dummy test once `import' actually working
-    it('should be an object', function() {
-        theseus.should.be.an('object');
+    it('should be a function', () => {
+        Http.should.be.a('function');
     });
 
+    describe('new Http', function() {
+        var http;
+
+        beforeEach(() => {
+            http = new Http;
+        });
+
+        it('should be an object', () => {
+            http.should.be.an('object');
+        });
+
+
+        describe('#get', function() {
+            it('should be a function', () => {
+                http.get.should.be.a('function');
+            });
+
+            // TODO: when called
+        });
+
+        // TODO: post
+        // TODO: put
+        // TODO: patch
+        // TODO: delete
+    });
 });
