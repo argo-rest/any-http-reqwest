641106313106e89934fb084423815dae45094d5a
Sébastien Cevey <seb.cevey@guardian.co.uk>
2014-11-09 17:01:58 +0000

    Initial karma test setup (not working yet)

---
+++ test/specs/theseus.spec.js
@@ -0,0 +1,10 @@
+import theseus from './index';
+
+describe('theseus', function() {
+
+    // TODO: replace dummy test once `import' actually working
+    it('should be an object', function() {
+        theseus.should.be.an('object');
+    });
+
+});
